98bae14409156a89f4574d502c3e8c35ff2ee693
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-06-25 19:38:16 +0000

    feat: Add JS RSA implementation

---
+++ pkg/algorithms/rsa.js
@@ -0,0 +1,57 @@
+const getRSAKeysImperative = (p, q) => {
+  const n = p * q;
+
+  const phi = (p - 1) * (q - 1);
+
+  let e = 2;
+  while (true) {
+    if (phi % e != 0) {
+      break;
+    }
+
+    e++;
+  }
+
+  let k = 1;
+  while (true) {
+    if ((k * phi + 1) % e == 0) {
+      break;
+    }
+
+    k++;
+  }
+
+  const d = Math.floor((k * phi + 1) / e);
+
+  return [n, e, d];
+};
+
+const rsaEncrypt = (m, e, n) => Math.pow(m, e) % n;
+
+const rsaDecrypt = (c, d, n) => Math.pow(c, d) % n;
+
+const getRSAKeysFunctional = (p, q, k, e) => {
+  /*
+  	To make this fully side-effect free, replace all references
+  	to the following with these expressions
+  */
+  const n = p * q;
+  const phi = (p - 1) * (q - 1);
+
+  if (phi % e != 0) {
+    if ((k * phi + 1) % e == 0) {
+      return [n, e, Math.floor((k * phi + 1) / e)];
+    }
+
+    return getRSAKeysFunctional(p, q, k + 1, e);
+  }
+
+  return getRSAKeysFunctional(p, q, k, e + 1);
+};
+
+module.exports = {
+  getRSAKeysImperative,
+  rsaEncrypt,
+  rsaDecrypt,
+  getRSAKeysFunctional,
+};
