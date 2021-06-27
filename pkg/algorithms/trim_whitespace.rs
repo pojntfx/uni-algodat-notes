38abe0e7f0d2f1bccd8c4326ab5bbb84c88c797e
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-06-27 17:59:28 +0000

    refactor: Trim whitespace without allocating a second string in Rust trimmer implementation

--- pkg/algorithms/trim_whitespace.rs
+++ pkg/algorithms/trim_whitespace.rs
@@ -1,14 +1,22 @@
 pub fn trim_whitespace_imperative(input: &str) -> String {
-    let mut output = String::new();
-
-    input.chars().for_each(|character| {
-        if !is_whitespace(character) {
-            output.push(character)
-        }
-    });
-
-    return output;
+    return input
+        .chars()
+        .filter(|character| !character.is_whitespace())
+        .collect();
 }
+
+// Alternative, less efficient solution
+// pub fn trim_whitespace_imperative(input: &str) -> String {
+// let mut output = String::new();
+//
+// input.chars().for_each(|character| {
+// if !is_whitespace(character) {
+// output.push(character)
+// }
+// });
+//
+// return output;
+// }
 
 pub fn trim_whitespace_functional(input: &str, output: String, i: usize) -> String {
     if i >= input.len() {
@@ -21,13 +29,9 @@ pub fn trim_whitespace_functional(input: &str, output: String, i: usize) -> Stri
     */
     let curr = input.chars().nth(i).unwrap();
 
-    if !is_whitespace(curr) {
+    if !curr.is_whitespace() {
         return trim_whitespace_functional(input, output + &curr.to_string(), i + 1);
     }
 
     return trim_whitespace_functional(input, output, i + 1);
-}
-
-fn is_whitespace(input: char) -> bool {
-    return input.is_whitespace();
 }
