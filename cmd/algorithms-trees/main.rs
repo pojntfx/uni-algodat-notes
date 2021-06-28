f8f742a28a91247b649cec141bc3c95d2de360fe
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-06-28 21:38:27 +0000

    feat: Start implementation of ADT tree in Rust

---
+++ cmd/algorithms-trees/main.rs
@@ -0,0 +1,18 @@
+#[path = "../../pkg/algorithms/trees.rs"]
+mod trees;
+
+use trees::Node;
+
+fn main() {
+    let mut n1 = Node::new(10);
+    let mut n2 = Node::new(11);
+    let n3 = Node::new(12);
+    let n4 = Node::new(13);
+    let n5 = Node::new(14);
+
+    n1.bin(None, Some(n5));
+    n2.bin(Some(n4), None);
+    n1.bin(Some(n2), Some(n3));
+
+    dbg!(n1);
+}
