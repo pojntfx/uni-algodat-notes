c562dc4355d490fa7aa69bee33871c1b14530dc4
Felix Pojtinger <felix@pojtinger.com>
2021-06-30 21:15:22 +0000

    feat: Add basic semi-functional quick sort implementation in Go

---
+++ cmd/algorithms-quick-sort/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pojntfx/uni-algodat-notes/pkg/algorithms"
+)
+
+func main() {
+	inputs := [][]int{
+		{1, 6, 10, 2, 11},
+		{2, 1, 2, 9, 3, 5},
+		{3, 38, 4, 45, 5, 7, 12, 50},
+	}
+
+	for _, input := range inputs {
+		outputImperative := algorithms.QuickSortImperative(input)
+		outputFunctional := algorithms.QuickSortFunctional(input)
+
+		fmt.Printf("in=%v out=%v=%v\n", input, outputImperative, outputFunctional)
+	}
+}
